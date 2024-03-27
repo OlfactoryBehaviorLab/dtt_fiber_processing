be5af2784a61d05a7f2919d5594b2e98697c525a
Austin Pauley <austin2118ace@gmail.com>
2024-03-27 15:40:40 -0400

    Parse all the items in the data folder and separate them into the query list and data

--- dtt_fiber_processing.py
+++ dtt_fiber_processing.py
@@ -18,10 +18,27 @@ def get_folder(default_dir='R:') -> Path:
     return file_path
 
 
+def parse_datafile_paths(data_dir) -> tuple:
+    query_file = []
+    data_dir_contents = list(data_dir.glob('*.csv')) # Get all CSV Files
+
+    for file in data_dir_contents: # Find the query file, set it aside, and remove it from the list
+        if 'query' in file.stem:
+            query_file = file
+            data_dir_contents.remove(file)
+
+    return query_file, data_dir_contents
+    
+
+
 
 def main():
+    #data_dir = get_folder()
 
-    
+    data_dir = Path('R:\\3_Histology\Qupath Projects\export measurements Test65')
+    query_file, data_files = parse_datafile_paths(data_dir)
+    data_file = data_files[0] # We're going to work with one file for now
+
 
 if __name__ == "__main__":
     main()
